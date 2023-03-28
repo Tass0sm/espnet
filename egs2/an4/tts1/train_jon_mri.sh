8607efa5daf6a4016f67407029329d3a2e515914
amin.karimi <amin.karimi1992@gmail.com>
2023-03-28 12:14:37 -0400

    add new job for mri cluster

---
+++ egs2/an4/tts1/train_jon_mri.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=tts_task
+#SBATCH --time=23:59:59
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH -p a100
+#SBATCH --gpus-per-node=0
+#SBATCH --mem=80gb
+
+cd espnet
+module spider cuda
+conda activate conda activate speech_project_env
+
+set -x
+
+source path.sh
+
+./run.sh --ngpu 1 --nj 16 --stage 6 --stop-stage 6 \
+         --train_config "conf/train_transformer.yaml"
