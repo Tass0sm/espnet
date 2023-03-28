90ee6d28f7b31eadc1434310d4674c879e1dca76
amin.karimi <amin.karimi1992@gmail.com>
2023-03-28 16:36:15 -0400

    cosine_embedding_loss is fixed

--- egs2/an4/tts1/train_job_mri.sh
+++ egs2/an4/tts1/train_job_mri.sh
@@ -7,9 +7,14 @@
 #SBATCH --gpus-per-node=0
 #SBATCH --mem=80gb
 
-cd espnet
+
+module load gcc-compatibility/10.3.0
+module load cmake/3.17.2
+module load cuda/10.2.89
+module load nccl/2.11.4
+module load python
 module spider cuda
-conda activate /home/karimimonsefi.1/espnet/tools/speech_project_env_1/envs/speech_project_env_1
+conda activate /home/karimimonsefi.1/espnet/tools/venv/envs/espnet
 
 set -x
 
