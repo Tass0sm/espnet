6a8d8263c64cd45d9d4ab8032f9e1ca800c32a17
amin.karimi <amin.karimi1992@gmail.com>
2023-03-30 01:58:00 -0400

    cosine_embedding_loss is fixed

--- espnet2/tts/transformer/transformer.py
+++ espnet2/tts/transformer/transformer.py
@@ -165,9 +165,11 @@ class TransformerLossWithASR(TransformerLoss):
         vocoder = build_vocoder_from_file(
             vocoder_config, vocoder_file, "cpu"
         )
+        # d.download_and_unpack("kamo-naoyuki/mini_an4_asr_train_raw_bpe_valid.acc.best")
 
         speech2text = Speech2Text.from_pretrained(
-            "Shinji Watanabe/librispeech_asr_train_asr_transformer_e18_raw_bpe_sp_valid.acc.best",
+            "kamo-naoyuki/mini_an4_asr_train_raw_bpe_valid.acc.best",
+            # "Shinji Watanabe/librispeech_asr_train_asr_transformer_e18_raw_bpe_sp_valid.acc.best",
             # Decoding parameters are not included in the model file
             maxlenratio=0.0,
             minlenratio=0.0,
