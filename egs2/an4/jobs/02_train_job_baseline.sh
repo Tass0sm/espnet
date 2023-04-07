454f72eaaea49cfab237049fa1e7a8a0ab9e4452
Tassos Manganaris <tassos.manganaris@gmail.com>
2023-04-07 00:54:00 -0400

    Add job and fix conf for more doable training

---
+++ egs2/an4/jobs/02_train_job_baseline.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=an4_train_job
+#SBATCH --time=16:00:00
+#SBATCH --nodes=1 --ntasks-per-node=16
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=4
+#SBATCH --account=pas2400
+#SBATCH --mail-type=ALL
+
+module load gcc-compatibility/10.3.0
+module load cmake/3.17.2
+module load cuda/10.2.89
+module load nccl/2.11.4
+module load python
+source activate espnet-env
+
+source path.sh
+
+set -x
+
+EXP=$(realpath ../tts-project-exp1)
+CONF=$(realpath ../utts1/conf/train_transformer.yaml)
+
+cd ../utts1
+
+./run.sh --ngpu 4 --nj 16 --stage 5 --stop-stage 6 \
+         --token_type char \
+         --cleaner none \
+         --g2p none \
+         --expdir $EXP \
+         --train_config $CONF
