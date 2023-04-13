287c9cafc5f4bede9981f7fb03a56c931f200d91
Tassos Manganaris <tassos.manganaris@gmail.com>
2023-04-13 11:51:43 -0400

    Add download_and_prep for ofuton_p_utagoe_db

---
+++ egs2/ofuton_p_utagoe_db/jobs/download_and_prep_job.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=opencpop_download_and_prep
+#SBATCH --time=8:00:00
+#SBATCH --nodes=1 --ntasks-per-node=16
+#SBATCH --cpus-per-task=1
+#SBATCH --account=pas2400
+#SBATCH --mail-type=ALL
+
+module load gcc-compatibility/10.3.0
+module load cmake/3.17.2
+module load cuda/10.2.89
+module load nccl/2.11.4
+module load python
+source activate espnet-env
+
+source path.sh
+
+set -x
+
+cd ../svs1
+
+./run.sh --ngpu 0 --nj 16 --stage 0 --stop-stage 4
